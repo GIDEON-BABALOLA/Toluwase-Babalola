1a59df946881fbdda2811170ce1be85010812344
GIDEON BABALOLA <gideonbabalola69@gmail.com>
2023-08-02 18:50:28 +0100

    Improved footer

--- public/index.js
+++ public/index.js
@@ -78,50 +78,5 @@ function player(){
     const compose = new Audio("audio/compose.mp3");
     compose.play();
   }
-  //Get in touch
-var words = ["Get in touch_"],
-part,
-i = 0,
-offset = 0,
-len = words.length,
-forwards = true,
-skip_count = 0,
-skip_delay = 15,
-speed = 70;
-var wordflex = function () {
-setInterval(function () {
-  if (forwards) {
-    if (offset >= words[i].length) {
-      ++skip_count;
-      if (skip_count == skip_delay) {
-        forwards = false;
-        skip_count = 0;
-      }
-    }
-  }
-  else {
-    if (offset == 0) {
-      forwards = true;
-      i++;
-      offset = 0;
-      if (i >= len) {
-        i = 0;
-      }
-    }
-  }
-  part = words[i].substr(0, offset);
-  if (skip_count == 0) {
-    if (forwards) {
-      offset++;
-    }
-    else {
-      offset--;
-    }
-  }
-  $('.getout').text(part);
-},speed);
-};
-$(document).ready(function () {
-wordflex();
-});
+
 
